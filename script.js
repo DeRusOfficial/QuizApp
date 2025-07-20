7e5dba9d8ae2d3c898f8f2f0418464857e7d2ab5
DeRusOfficial <derus.ru@mail.ru>
2025-07-20 10:00:41 +0300

    JSON File

---
+++ script.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        "question": "Was ist die Hauptfunktion eines Betriebssystems?",
+        "answers": [
+            "Dokumente automatisch speichern",
+            "Internetverbindungen beschleunigen",
+            "Hardwarekomponenten steuern und Ressourcen verwalten",
+            "Grafiken für Spiele rendern"
+            ],
+        "rightAnswer": 2
+    },
+    {
+        "question": "Welches dieser Konzepte ist ein grundlegendes Prinzip der objektorientierten Programmierung?",
+        "answers": [
+            "Vererbung",
+            "Lineare Suche",
+            "Globaler Variableneinsatz",
+            "Prozedurale Programmierung"
+            ],
+        "rightAnswer": 0
+    },
+    {
+        "question": "Was beschreibt der Begriff 'Big O Notation' in der Informatik?",
+        "answers": [
+            "Die Größe eines Datenspeichers",
+            "Die Laufzeitkomplexität eines Algorithmus",
+            "Die Version eines Betriebssystems",
+            "Die Geschwindigkeit einer Internetverbindung"
+            ],
+        "rightAnswer": 1
+    },
+    {
+        "question": "Welches Protokoll wird typischerweise für die sichere Übertragung von Daten im Internet verwendet?",
+        "answers": [
+            "FTP",
+            "HTTP",
+            "SMTP",
+            "HTTPS"
+            ],
+        "rightAnswer": 3
+    },
+    {
+        "question": "Was ist ein 'Stack' in der Informatik?",
+        "answers": [
+            "Eine Datenstruktur nach dem LIFO-Prinzip",
+            "Ein spezieller Typ einer Datenbank",
+            "Ein Algorithmus zur Sortierung",
+            "Ein Maß für die Rechenleistung"
+            ],
+        "rightAnswer": 0
+    }
+]
